f1b506f8ce1614b1b7c5176193e05e55c11dae5c
grivo <gyket.pentest.133971@protonmail.com>
2024-10-11 14:38:56 -0300

    estruturas globais do nping

---
+++ tools/nmap/nping/global_structures.h
@@ -0,0 +1,29 @@
+#ifndef GLOBAL_STRUCTURES_H
+#define GLOBAL_STRUCTURES_H
+
+#include "nsock.h"
+#include "nbase.h"
+
+typedef struct service_lookup {
+    char *name;
+    u8 proto;
+    u16 portno;
+} service_lookup;
+
+
+typedef struct m_data {
+    nsock_pool nsp;
+	nsock_iod nsi;
+	int state;
+	int protocol;
+	unsigned short port;
+	struct in_addr ip;
+	int attempts;
+    int max_attempts; /* tentativas em uma conexão */
+    char *username;
+	char *password;
+	char *buf;
+	int bufsize;
+} m_data;
+
+#endif
